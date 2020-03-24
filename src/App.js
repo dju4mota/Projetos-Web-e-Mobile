5ffd0a0739e8d7f898ebf2982ab0d782983883a0
dju4mota <fernando.b@ges.inatel.com>
2020-03-24 12:15:35 -0300

    Lógica das operações

--- src/App.js
+++ src/App.js
@@ -4,15 +4,66 @@ import Botao from './componentes/Botao'
 import Styles from './estilos/Padrao'
 import Display from './componentes/Display'
 
+const initalState={
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0,0],
+  current: 0,
+}
+  
+
 export default class App extends Component {
-  state = {
-    displayValue: '0'
+  state = {...initalState}
+
+  addDigit = n => {
+
+    const clearDisplay = this.state.displayValue === '0' 
+      || this.state.clearDisplay    
+
+    if(n=== '.' && !clearDisplay && this.state.displayValue.includes('.')){
+      return
+    }
+
+    const currentValue = clearDisplay ? '': this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({displayValue,clearDisplay: false})
+
+    if (n !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({values})
+    }
   }
 
-  addDigit = n => this.setState({displayValue: n})
-  clearMemory = () => this.setState({displayValue: '0'})
-  setOperation = operation => {
 
+  clearMemory = () => {
+    this.setState({...initalState})
+  }
+
+  setOperation = operation => {
+    if(this.state.current === 0){
+      this.setState({operation,current:1,clearDisplay:true})
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try{
+        values[0] = 
+        eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      }catch(e){
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0: 1,
+        clearDisplay: true,  // !equals 
+        values,
+      })
+    }
   }
 
   render() {
@@ -21,7 +72,7 @@ export default class App extends Component {
       <Display value={this.state.displayValue} />
       <View style={Styles.botoes}>
         <Botao label= 'AC' triple onClick={this.clearMemory}/>
-        <Botao label= '/'  operation onClick={this.setOperation}/>
+        <Botao label= '/' operation onClick={this.setOperation}/>
         <Botao label= '7' onClick={this.addDigit}/>
         <Botao label= '8' onClick={this.addDigit}/>
         <Botao label= '9' onClick={this.addDigit}/>
@@ -36,8 +87,9 @@ export default class App extends Component {
         <Botao label= '+' operation onClick={this.setOperation}/>
         <Botao label= '0' double onClick={this.addDigit}/>
         <Botao label= '.' onClick={this.addDigit}/>
-        <Botao label= '='operation />
+        <Botao label= '=' operation onClick={this.setOperation}/>
       </View>
     </View>
-  );
-}}
+  );  
+  }
+}
